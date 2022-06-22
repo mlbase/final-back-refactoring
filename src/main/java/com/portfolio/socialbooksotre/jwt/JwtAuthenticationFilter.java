279d99bfd45c39f0712838644098ccc3bb75bd33
mlbase <guddnr2424@gmail.com>
2022-06-22 19:30:16 +0900

    jwt 연습중

--- src/main/java/com/portfolio/socialbooksotre/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/portfolio/socialbooksotre/jwt/JwtAuthenticationFilter.java
@@ -2,6 +2,7 @@ package com.portfolio.socialbooksotre.jwt;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -19,6 +20,21 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         log.info("jwt Filter");
+        String jwtToken = parseJwt(request);
+        log.info("jwtToken: {}", jwtToken);
+        if(jwtToken != null){
 
+        }
+        log.info("next Filter");
+        filterChain.doFilter(request, response);
+
+    }
+
+    private String parseJwt(HttpServletRequest request) {
+        String headerAuth = request.getHeader("Authorization");
+        if(StringUtils.hasText(headerAuth) && headerAuth.startsWith("Bearer ")){
+            return headerAuth.substring(7, headerAuth.length());
+        }
+        return null;
     }
 }
---
+++ src/main/java/com/portfolio/socialbooksotre/users/dto/UsersRegister.java
@@ -0,0 +1,4 @@
+package com.portfolio.socialbooksotre.users.dto;
+
+public class UsersRegister {
+}
